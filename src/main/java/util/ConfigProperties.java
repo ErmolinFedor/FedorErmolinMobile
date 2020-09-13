9f76387a776ff09d04f6cd0ad4948c54192228d7
fbepdor@yandex.ru <itWQxzislb28>
2020-09-13 18:41:28 +0300

    Initial commit

---
+++ src/main/java/util/ConfigProperties.java
@@ -0,0 +1,26 @@
+package util;
+
+import setup.BaseTest;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+public class ConfigProperties {
+    private static java.util.Properties properties;
+
+    static {
+        properties = new java.util.Properties();
+        try (InputStream inputStream = BaseTest.class.getClassLoader()
+                .getResourceAsStream("config.properties")) {
+            properties.load(inputStream);
+        } catch (IOException e) {
+            System.out.println(e);
+        }
+    }
+
+    private ConfigProperties() {}
+
+    public static String getProperty(String propertyName) {
+        return properties.getProperty(propertyName);
+    }
+}
---
+++ src/test/java/scenarios/nativeMobileTests.java
@@ -0,0 +1,27 @@
+package scenarios;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import setup.BaseTest;
+import util.ConfigProperties;
+
+public class nativeMobileTests extends BaseTest{
+    @Test(groups = {"native"}, description = "This simple test just click on the Sign In button")
+    public void simpleNativeTest() throws IllegalAccessException, NoSuchFieldException, InstantiationException, InterruptedException {
+
+        getPo().getWelement("registerButton").click();
+        getPo().getWelement("registrationEmail").sendKeys(ConfigProperties.getProperty("Email"));
+        getPo().getWelement("registrationUserName").sendKeys(ConfigProperties.getProperty("UserName"));
+        getPo().getWelement("registrationPassword").sendKeys(ConfigProperties.getProperty("Password"));
+        getPo().getWelement("registrationConfirmPassword").sendKeys(ConfigProperties.getProperty("Password"));
+        getPo().getWelement("registerNewAccountButton").click();
+        getPo().getWelement("loginEmail").sendKeys(ConfigProperties.getProperty("Email"));
+        getPo().getWelement("loginPassword").sendKeys(ConfigProperties.getProperty("Password"));
+        getPo().getWelement("signInButton").click();
+        String pageText = getPo().getWelement("pageText").getText();
+        System.out.println(pageText);
+        Assert.assertEquals(pageText, "BudgetActivity");
+        System.out.println("Simple Android native test done");
+
+    }
+}
